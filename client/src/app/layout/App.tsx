3e00c34b6a1e19dc4fc74ef262c5a87281ff49a3
Billy Walton <BillyW@magenic.com>
2022-06-18 11:33:38 -0500

    End of section 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,12 @@
 import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import { Container } from '@mui/system';
 import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import About from '../../features/about/About';
 import Catalog from '../../features/catalog/Catalog';
+import ProductDetails from '../../features/catalog/ProductDetails';
+import Contact from '../../features/contact/Contact';
+import Home from '../../features/home/Home';
 import Header from './Header';
 
 const App = () => {
@@ -26,7 +31,13 @@ const App = () => {
       <CssBaseline />
       <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <Container>
-        <Catalog />
+        <Routes>
+          <Route path='/' element={ <Home /> } />
+          <Route path='/catalog' element={ <Catalog /> } />
+          <Route path='/catalog/:id' element={ <ProductDetails /> } />
+          <Route path='/about' element={ <About /> } />
+          <Route path='/contact' element={ <Contact /> } />
+        </Routes>
       </Container>
     </ThemeProvider>
   );
---
+++ client/src/features/contact/Contact.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const Contact = () => {
+    return (
+        <Typography variant='h2'>
+            Contact Page
+        </Typography>
+    );
+};
+
+export default Contact;
